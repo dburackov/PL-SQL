07de66987df22e9203da5124ccd99b64e8c95c11
dburackov <dmburackov@yandex.by>
2023-02-08 14:20:14 +0300

    lab1 done

--- lab1/lab1.sql
+++ lab1/lab1.sql
@@ -1,3 +1,4 @@
+SET SERVEROUTPUT ON;
 --Task 1
 CREATE TABLE MyTable
 (
@@ -8,7 +9,7 @@ CREATE TABLE MyTable
 
 --Task 2
 DECLARE
-    N CONSTANT NUMBER := 10;
+    N CONSTANT NUMBER := 10000;
 BEGIN
     FOR i IN 1..N
         LOOP
@@ -16,6 +17,10 @@ BEGIN
             VALUES (i, DBMS_RANDOM.RANDOM());
         END LOOP;
 END;
+
+
+SELECT COUNT(*) FROM MyTable;
+SELECT id, val FROM MyTable ORDER BY id FETCH FIRST 10 ROWS ONLY;
 
 
 --Task 3
@@ -44,8 +49,15 @@ BEGIN
         result := 'EQUAL';
     END IF;
 
+    DBMS_OUTPUT.PUT_LINE(even_cnt);
+    DBMS_OUTPUT.PUT_LINE(odd_cnt);
+
     RETURN result;
 END;
+
+
+SELECT F2 FROM DUAL;
+
 
 --Task 4
 CREATE OR REPLACE FUNCTION get_insert_query(id NUMBER, val NUMBER := DBMS_RANDOM.RANDOM())
@@ -65,12 +77,24 @@ BEGIN
 END;
 
 
+BEGIN
+    insert_query(12, 12);
+END;
+SELECT * FROM MyTable WHERE id=12;
+
+
 CREATE OR REPLACE PROCEDURE update_query(id NUMBER, val NUMBER := DBMS_RANDOM.RANDOM())
     IS
     table_name CONSTANT CHAR(7) := 'MyTable';
 BEGIN
     EXECUTE IMMEDIATE UTL_LMS.FORMAT_MESSAGE('UPDATE %s SET val=%d WHERE id=%d', table_name, TO_CHAR(val), TO_CHAR(id));
 END;
+
+
+BEGIN
+    update_query(12, 13);
+END;
+SELECT * FROM MyTable WHERE id=12;
 
 
 CREATE OR REPLACE PROCEDURE delete_query(id NUMBER)
@@ -81,12 +105,35 @@ BEGIN
 END;
 
 
+BEGIN
+    delete_query(12);
+END;
+SELECT * FROM MyTable WHERE id=12;
+
+
 --Task 6
 CREATE OR REPLACE FUNCTION get_total_remuneration(salary NUMBER, annual_percentage_rate NUMBER)
+RETURN NUMBER
 IS
+    incorrect_value EXCEPTION;
 BEGIN
-    
+    IF annual_percentage_rate < 0 OR salary < 0 THEN
+        RAISE incorrect_value;
+    END IF;
+
+    RETURN (1 + annual_percentage_rate / 100) * 12 * salary;
+
+    EXCEPTION
+        WHEN incorrect_value THEN
+            RETURN NULL;
+        WHEN INVALID_NUMBER THEN
+            RETURN NULL;
+        WHEN VALUE_ERROR THEN
+            RETURN NULL;
 END;
+
+
+SELECT get_total_remuneration(81724, 1234) FROM DUAL;
 
 
 DELETE
