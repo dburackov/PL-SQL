e848a6864e32d1db53258af27c338775f1f3776c
dburackov <dmburackov@yandex.by>
2023-03-09 12:07:13 +0300

    lab2 task1

---
+++ lab2/lab2.sql
@@ -0,0 +1,18 @@
+-- Task 1 ____________________________________________
+
+CREATE TABLE STUDENTS
+(
+    ID NUMBER PRIMARY KEY ,
+    NAME VARCHAR2(50) NOT NULL,
+    GROUP_ID NUMBER NOT NULL
+);
+
+CREATE TABLE GROUPS
+(
+    ID NUMBER PRIMARY KEY ,
+    NAME VARCHAR2(50) NOT NULL,
+    C_VAL NUMBER NOT NULL
+);
+
+-- Task 2 ____________________________________________
+
