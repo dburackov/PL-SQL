e339e842a4a789002892ee600a13ad032cc41d88
dburackov <dmburackov@yandex.by>
2023-04-14 20:58:11 +0300

    generate ddl scripts for create tables

--- lab3/table_comp.sql
+++ lab3/table_comp.sql
@@ -1,5 +1,6 @@
 CREATE OR REPLACE PROCEDURE PROD_CREATE_LIST(dev_schema_name VARCHAR2, prod_schema_name VARCHAR2)
     IS
+    counter NUMBER(10);
 BEGIN
     FOR diff IN (Select DISTINCT table_name
                  from all_tab_columns
@@ -7,7 +8,29 @@ BEGIN
                    and (table_name, column_name) not in
                        (select table_name, column_name from all_tab_columns where owner = prod_schema_name))
         LOOP
-            DBMS_OUTPUT.PUT_LINE(diff.TABLE_NAME);
+            counter := 0;
+
+            SELECT COUNT(*)
+            INTO counter
+            FROM all_tables
+            where owner = prod_schema_name
+              and table_name = diff.table_name;
+
+            IF counter > 0 THEN
+                FOR res2 IN (Select DISTINCT column_name, data_type
+                             from all_tab_columns
+                             where owner = dev_schema_name
+                               and table_name = diff.table_name
+                               and (table_name, column_name) not in
+                                   (select table_name, column_name from all_tab_columns where owner = prod_schema_name))
+                    LOOP
+                        DBMS_OUTPUT.PUT_LINE('ALTER TABLE ' || prod_schema_name || '.' || diff.table_name || ' ADD ' ||
+                                             res2.column_name || ' ' || res2.data_type || ';');
+                    END LOOP;
+            ELSE
+                DBMS_OUTPUT.PUT_LINE('CREATE TABLE ' || prod_schema_name || '.' || diff.table_name ||
+                                     ' AS (SELECT * FROM ' || dev_schema_name || '.' || diff.table_name || ');');
+            END IF;
         END LOOP;
 END;
 
