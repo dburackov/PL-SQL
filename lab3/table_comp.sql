22e6b8a548764be78e8782c1ac815f19c628cadd
dburackov <dmburackov@yandex.by>
2023-04-15 14:27:02 +0300

    ddl scripts reform

--- lab3/table_comp.sql
+++ lab3/table_comp.sql
@@ -37,6 +37,8 @@ END;
 
 CREATE OR REPLACE PROCEDURE PROD_DELETE_LIST(dev_schema_name VARCHAR2, prod_schema_name VARCHAR2)
     IS
+    counter  NUMBER(10);
+    counter2 NUMBER(10);
 BEGIN
     FOR diff IN (Select DISTINCT table_name
                  from all_tab_columns
@@ -44,7 +46,39 @@ BEGIN
                    and (table_name, column_name) not in
                        (select table_name, column_name from all_tab_columns where owner = dev_schema_name))
         LOOP
-            DBMS_OUTPUT.PUT_LINE(diff.TABLE_NAME);
+            counter := 0;
+            counter2 := 0;
+
+            SELECT COUNT(column_name)
+            INTO counter
+            FROM all_tab_columns
+            where owner = prod_schema_name
+              and table_name = diff.table_name;
+
+            SELECT COUNT(column_name)
+            INTO counter2
+            FROM all_tab_columns
+            where owner = dev_schema_name
+              and table_name = diff.table_name;
+
+            IF counter != counter2 THEN
+                FOR res2 IN (select column_name
+                             from all_tab_columns
+                             where owner = prod_schema_name
+                               and table_name = diff.table_name
+                               and column_name not in (select column_name
+                                                       from all_tab_columns
+                                                       where owner = dev_schema_name
+                                                         and table_name = diff.table_name))
+                    LOOP
+                        DBMS_OUTPUT.PUT_LINE('ALTER TABLE ' || prod_schema_name || '.' || diff.table_name ||
+                                             ' DROP COLUMN ' || res2.column_name || ';');
+                    END LOOP;
+
+            ELSE
+                DBMS_OUTPUT.PUT_LINE('DROP TABLE ' || prod_schema_name || '.' || diff.table_name ||
+                                     ' CASCADE CONSTRAINTS;');
+            END IF;
         END LOOP;
 END;
 
