bf0851a53b8ee99b36318baa09c080e1d2af5852
dburackov <dmburackov@yandex.by>
2023-04-19 17:13:04 +0300

    procedure comp final fix

--- lab3/procedure_comp.sql
+++ lab3/procedure_comp.sql
@@ -1,4 +1,5 @@
 CREATE OR REPLACE PROCEDURE PROD_PROCEDURE_CREATE(dev_schema_name VARCHAR2, prod_schema_name VARCHAR2)
+    AUTHID CURRENT_USER
     IS
     counter NUMBER(10);
 BEGIN
@@ -9,7 +10,8 @@ BEGIN
                    and object_name not in
                        (select object_name
                         from all_objects
-                        where owner = prod_schema_name and object_type = 'PROCEDURE'))
+                        where owner = prod_schema_name
+                          and object_type = 'PROCEDURE'))
         LOOP
             counter := 0;
             DBMS_OUTPUT.PUT_LINE('CREATE OR REPLACE ');
@@ -19,10 +21,12 @@ BEGIN
                            and name = diff.object_name
                            and owner = dev_schema_name)
                 LOOP
-                    IF COUNTER != 0 THEN
-                        DBMS_OUTPUT.PUT_LINE(rtrim(res2.text, chr(10) || chr(13)));
+                    IF counter != 0 THEN
+                        DBMS_OUTPUT.PUT_LINE(RTRIM(res2.text, CHR(10) || CHR(13)));
                     ELSE
-                        DBMS_OUTPUT.PUT_LINE(rtrim(prod_schema_name || '.' || res2.text, chr(10) || chr(13)));
+                        DBMS_OUTPUT.PUT_LINE(RTRIM(
+                                REPLACE('PROCEDURE ' || prod_schema_name || '.' || SUBSTR(res2.text, 10), ' ', ''),
+                                CHR(10) || CHR(13)));
                         counter := 1;
                     END IF;
                 END LOOP;
@@ -30,6 +34,7 @@ BEGIN
 END;
 
 CREATE OR REPLACE PROCEDURE PROD_PROCEDURE_DELETE(dev_schema_name VARCHAR2, prod_schema_name VARCHAR2)
+    AUTHID CURRENT_USER
     IS
 BEGIN
     FOR diff IN (select DISTINCT object_name
@@ -42,3 +47,39 @@ BEGIN
             DBMS_OUTPUT.PUT_LINE('DROP PROCEDURE ' || prod_schema_name || '.' || diff.object_name);
         END LOOP;
 END;
+
+CREATE OR REPLACE PROCEDURE PROD_PROCEDURE_DELETE_CREATE(dev_schema_name VARCHAR2, prod_schema_name VARCHAR2)
+    AUTHID CURRENT_USER
+    IS
+    counter NUMBER(10);
+BEGIN
+    FOR diff IN (SELECT DISTINCT object_name
+                 FROM all_objects
+                 WHERE object_type = 'PROCEDURE'
+                   AND OWNER = dev_schema_name
+                   AND object_name IN
+                       (SELECT object_name
+                        FROM all_objects
+                        WHERE OWNER = prod_schema_name AND object_type = 'PROCEDURE'))
+        LOOP
+            counter := 0;
+            DBMS_OUTPUT.PUT_LINE('DROP PROCEDURE ' || prod_schema_name || '.' || diff.object_name || ';');
+            DBMS_OUTPUT.PUT_LINE('CREATE OR REPLACE ');
+            FOR body IN (SELECT text
+                         FROM all_source
+                         WHERE type = 'PROCEDURE'
+                           AND name = diff.object_name
+                           AND Owner = dev_schema_name)
+                LOOP
+                    IF counter != 0 THEN
+                        DBMS_OUTPUT.PUT_LINE(RTRIM(body.text, CHR(10) || CHR(13)));
+                    ELSE
+                        DBMS_OUTPUT.PUT_LINE(RTRIM(
+                                REPLACE('PROCEDURE ' || prod_schema_name || '.' || SUBSTR(body.text, 10), ' ', ''),
+                                CHR(10) || CHR(13)));
+                        counter := 1;
+                    END IF;
+                END LOOP;
+        END LOOP;
+END;
+
